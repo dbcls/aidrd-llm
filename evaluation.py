afcbda3c89745e3c76fd72e69c2dd8a002fc2281
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2024-09-10 15:19:07 +0900

    feat: use open_ai_callback to count token usage for evaluation

--- evaluation.py
+++ evaluation.py
@@ -12,6 +12,7 @@ from typing import List, Tuple
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_community.callbacks.manager import get_openai_callback
 
 from langchain_openai import AzureChatOpenAI
 
@@ -22,7 +23,7 @@ def query_to_api(query):
     url = f"{os.environ.get('API_BASE_URL')}/chat-messages"
     headers = {
         "Authorization": f"Bearer {os.environ.get('CHATBOT_API_KEY')}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     data = {
         "inputs": {},
@@ -34,10 +35,11 @@ def query_to_api(query):
     answer = response["answer"]
     if "metadata" not in response or "retriever_resources" not in response["metadata"]:
         return answer, []
-    soucre_url_list = [resource["document_name"] for resource in response["metadata"]["retriever_resources"]]
+    soucre_url_list = [
+        resource["document_name"]
+        for resource in response["metadata"]["retriever_resources"]
+    ]
     return answer, soucre_url_list
-
-
 
 
 def evaluate_by_llm(expected_answer, actual_answer):
@@ -45,11 +47,12 @@ def evaluate_by_llm(expected_answer, actual_answer):
         azure_deployment=os.environ.get("AZURE_DEPLOYMENT_ID"),
         api_version="2024-05-01-preview",
         temperature=0.4,
-        max_retries=3
+        max_retries=3,
     )
 
     class EvaluationResult(BaseModel):
         similarity: int = Field(description="The similarity score from 1 to 5")
+
     query = f"""e
 Here we have two answers. The first answer is the expected answer and the second answer is the actual answer.
 Please evaluate the similarity actual answer based on the expected answer and compute the score from 1 to 5.
@@ -70,8 +73,6 @@ Below are the details for different scores:
 {actual_answer}
 ```
 """
-    
-    
 
     parser = PydanticOutputParser(pydantic_object=EvaluationResult)
 
@@ -84,85 +85,121 @@ Below are the details for different scores:
     chain = prompt | chat_model | parser
 
     evaluation_result = chain.invoke({"query": query})
-    return evaluation_result.similarity 
+    return evaluation_result.similarity
 
 
 if __name__ == "__main__":
-    with open("evaluation_data.json") as f:
-        evaluation_data = json.load(f)
 
-    source_url_accuracy = {
-        "true_positive": 0, 
-        "false_positive": 0,
-        "false_negative": 0,
-    }
-    total = 0
-    similarity_score_sum = 0
+    with get_openai_callback() as cb:
+        with open("evaluation_data.json") as f:
+            evaluation_data = json.load(f)
 
-    # get current date and time for csv file name
-    current_date_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        source_url_accuracy = {
+            "true_positive": 0,
+            "false_positive": 0,
+            "false_negative": 0,
+        }
+        total = 0
+        similarity_score_sum = 0
 
-    csv_file = open(f"evaluation_results_{current_date_time}.csv", mode="w", newline="", encoding="utf-8-sig")
-    fieldnames = ["Number", "query", "expected_source_urls", "actual_source_urls", "source_urls_f1_score", "expected_answer", "actual_answer", "answer_similarity"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
+        # get current date and time for csv file name
+        current_date_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    for data in evaluation_data:
-        total += 1
-        query = data["query"]
-        expected_answer = data["expected_answer"]
-        expected_source_url_list = data["source_url_list"]
-        actual_answer, actual_source_url_list = query_to_api(query)
-        actual_source_url_list = list(set(actual_source_url_list))
-        local_true_positive = 0
-        local_false_positive = 0
-        local_false_negative = 0
-        for source_url in expected_source_url_list:
-            if source_url in actual_source_url_list:
-                local_true_positive += 1
-            else:
-                local_false_negative += 1
-        for source_url in actual_source_url_list:
-            if source_url not in expected_source_url_list:
-                local_false_positive += 1
-        source_url_accuracy["true_positive"] += local_true_positive
-        source_url_accuracy["false_positive"] += local_false_positive
-        source_url_accuracy["false_negative"] += local_false_negative
-        source_f1_score = 2 * local_true_positive / (2 * local_true_positive + local_false_positive + local_false_negative)
-        answer_similarity_score = evaluate_by_llm(expected_answer, actual_answer)
-        similarity_score_sum += answer_similarity_score
-        print(f"Query: {query}")
-        print(f"Expected Source URLs: {expected_source_url_list}")
-        print(f"Source URLs: {actual_source_url_list}")
-        print(f"Source F1 Score: {source_f1_score}")        
-        print(f"Expected Answer: {expected_answer}")
-        print(f"Retrieved Answer: {actual_answer}")
-        print(f"Answer Similarity Score: {answer_similarity_score}")
-        print("") # For delimiter
+        csv_file = open(
+            f"evaluation_results_{current_date_time}.csv",
+            mode="w",
+            newline="",
+            encoding="utf-8-sig",
+        )
+        fieldnames = [
+            "Number",
+            "query",
+            "expected_source_urls",
+            "actual_source_urls",
+            "source_urls_f1_score",
+            "expected_answer",
+            "actual_answer",
+            "answer_similarity",
+        ]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
 
-        writer.writerow({
-            "Number": total,
-            "query": query,
-            "expected_source_urls": '\n'.join(expected_source_url_list),
-            "actual_source_urls": '\n'.join(actual_source_url_list),
-            "source_urls_f1_score": source_f1_score,
-            "expected_answer": expected_answer,
-            "actual_answer": actual_answer,
-            "answer_similarity": answer_similarity_score
-        })
+        for data in evaluation_data:
+            total += 1
+            query = data["query"]
+            expected_answer = data["expected_answer"]
+            expected_source_url_list = data["source_url_list"]
+            actual_answer, actual_source_url_list = query_to_api(query)
+            actual_source_url_list = list(set(actual_source_url_list))
+            local_true_positive = 0
+            local_false_positive = 0
+            local_false_negative = 0
+            for source_url in expected_source_url_list:
+                if source_url in actual_source_url_list:
+                    local_true_positive += 1
+                else:
+                    local_false_negative += 1
+            for source_url in actual_source_url_list:
+                if source_url not in expected_source_url_list:
+                    local_false_positive += 1
+            source_url_accuracy["true_positive"] += local_true_positive
+            source_url_accuracy["false_positive"] += local_false_positive
+            source_url_accuracy["false_negative"] += local_false_negative
+            source_f1_score = (
+                2
+                * local_true_positive
+                / (
+                    2 * local_true_positive
+                    + local_false_positive
+                    + local_false_negative
+                )
+            )
+            answer_similarity_score = evaluate_by_llm(expected_answer, actual_answer)
+            similarity_score_sum += answer_similarity_score
+            print(f"Query: {query}")
+            print(f"Expected Source URLs: {expected_source_url_list}")
+            print(f"Source URLs: {actual_source_url_list}")
+            print(f"Source F1 Score: {source_f1_score}")
+            print(f"Expected Answer: {expected_answer}")
+            print(f"Retrieved Answer: {actual_answer}")
+            print(f"Answer Similarity Score: {answer_similarity_score}")
+            print("")  # For delimiter
 
+            writer.writerow(
+                {
+                    "Number": total,
+                    "query": query,
+                    "expected_source_urls": "\n".join(expected_source_url_list),
+                    "actual_source_urls": "\n".join(actual_source_url_list),
+                    "source_urls_f1_score": source_f1_score,
+                    "expected_answer": expected_answer,
+                    "actual_answer": actual_answer,
+                    "answer_similarity": answer_similarity_score,
+                }
+            )
 
-    f1_score = 2 * source_url_accuracy["true_positive"] / (2 * source_url_accuracy["true_positive"] + source_url_accuracy["false_positive"] + source_url_accuracy["false_negative"])
-    print(f"Total F1 Score: {f1_score}")
-    print(f"Average Answer Similarity Score: {similarity_score_sum / total}")
-    writer.writerow({
-        "Number": "Total",
-        "query": "",
-        "expected_source_urls": "",
-        "actual_source_urls": "",
-        "source_urls_f1_score": f1_score,
-        "expected_answer": "",
-        "actual_answer": "",
-        "answer_similarity": similarity_score_sum / total
-    })
-    csv_file.close()
+        f1_score = (
+            2
+            * source_url_accuracy["true_positive"]
+            / (
+                2 * source_url_accuracy["true_positive"]
+                + source_url_accuracy["false_positive"]
+                + source_url_accuracy["false_negative"]
+            )
+        )
+        print(f"Total F1 Score: {f1_score}")
+        print(f"Average Answer Similarity Score: {similarity_score_sum / total}")
+        writer.writerow(
+            {
+                "Number": "Total",
+                "query": "",
+                "expected_source_urls": "",
+                "actual_source_urls": "",
+                "source_urls_f1_score": f1_score,
+                "expected_answer": "",
+                "actual_answer": "",
+                "answer_similarity": similarity_score_sum / total,
+            }
+        )
+        csv_file.close()
+        print(cb)
